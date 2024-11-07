85480958d21896fd6939bf0002582cbe47bf8000
Matthew Sells <matthew.sells@proton.me>
2024-11-06 21:25:59 -0600

    format files

--- assets/svelte/Technique.svelte
+++ assets/svelte/Technique.svelte
@@ -1,20 +1,20 @@
 <script>
-	// export let live;
-	// export let technique = {};
+  // export let live;
+  // export let technique = {};
 
-	let name = '';
+  let name = "";
 </script>
 
 <form autocomplete="off">
-	<div class="flex flex-col">
-		<input
-			aria-label="Technique name"
-			bind:value={name}
-			placeholder="Butterfly Sweep"
-			class={[
-				"text-6xl px-3 py-2 placeholder:text-neutral-500",
-				"text-neutral-900 dark:text-neutral-300 bg-none bg-transparent"
-			]} 
-		/>
-	</div>
+  <div class="flex flex-col">
+    <input
+      aria-label="Technique name"
+      bind:value={name}
+      placeholder="Butterfly Sweep"
+      class={[
+        "text-6xl px-3 py-2 placeholder:text-neutral-500",
+        "text-neutral-900 dark:text-neutral-300 bg-none bg-transparent",
+      ]}
+    />
+  </div>
 </form>
