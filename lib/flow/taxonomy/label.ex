d0307e5c772e0a1099d1d7818075f6408fe39ef2
Matthew Sells <matthew.sells@proton.me>
2024-11-22 21:52:37 -0600

    add labels schema

---
+++ lib/flow/taxonomy/label.ex
@@ -0,0 +1,20 @@
+defmodule Flow.Taxonomy.Label do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "taxonomy_labels" do
+    field :tag, :string
+
+    belongs_to :user, Flow.Accounts.User
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(label, attrs) do
+    label
+    |> cast(attrs, [:tag])
+    |> validate_required([:tag])
+    |> unique_constraint([:user_id, :tag])
+  end
+end
---
+++ priv/repo/migrations/20241123034620_create_taxonomy_labels.exs
@@ -0,0 +1,15 @@
+defmodule Flow.Repo.Migrations.CreateTaxonomyLabels do
+  use Ecto.Migration
+
+  def change do
+    create table(:taxonomy_labels) do
+      add :tag, :string, null: false
+      add :user_id, references(:accounts_users, on_delete: :delete_all), null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:taxonomy_labels, [:user_id])
+    create unique_index(:taxonomy_labels, [:user_id, :tag])
+  end
+end
