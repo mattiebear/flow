1ca20428e4dc82ec7fac71933699476612708c49
Matthew Sells <matthew.sells@proton.me>
2024-12-08 20:48:32 -0600

    remove svelte

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,71 +16,69 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import 'phoenix_html';
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import { Socket } from 'phoenix';
-import { LiveSocket } from 'phoenix_live_view';
-import topbar from '../vendor/topbar';
-import { getHooks } from 'live_svelte';
-import * as Components from '../svelte/**/*.svelte';
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
 let Hooks = {
-	AutoResizeTextarea: {
-		mounted() {
-			this.el.style.height = this.el.scrollHeight + 'px';
-			this.el.style.overflowY = 'hidden';
+  AutoResizeTextarea: {
+    mounted() {
+      this.el.style.height = this.el.scrollHeight + "px";
+      this.el.style.overflowY = "hidden";
 
-			this.el.addEventListener('input', () => {
-				this.el.style.height = 'auto';
-				this.el.style.height = this.el.scrollHeight + 'px';
-			});
-		},
-	},
-	ModeToggle: {
-		mounted() {
-			const STORAGE_KEY = 'mode-toggle';
+      this.el.addEventListener("input", () => {
+        this.el.style.height = "auto";
+        this.el.style.height = this.el.scrollHeight + "px";
+      });
+    },
+  },
+  ModeToggle: {
+    mounted() {
+      const STORAGE_KEY = "mode-toggle";
 
-			let mode =
-				localStorage.getItem(STORAGE_KEY) === 'dark' ? 'dark' : 'light';
-			let input = this.el.querySelector('input');
-			let root = document.querySelector('html');
+      let mode =
+        localStorage.getItem(STORAGE_KEY) === "dark" ? "dark" : "light";
+      let input = this.el.querySelector("input");
+      let root = document.querySelector("html");
 
-			let setMode = (mode) => {
-				localStorage.setItem(STORAGE_KEY, mode);
-				root.className = mode;
-				this.el.dataset.mode = mode;
-			};
+      let setMode = (mode) => {
+        localStorage.setItem(STORAGE_KEY, mode);
+        root.className = mode;
+        this.el.dataset.mode = mode;
+      };
 
-			setMode(mode);
+      setMode(mode);
 
-			if (!input) {
-				return;
-			}
+      if (!input) {
+        return;
+      }
 
-			input.checked = mode === 'dark';
+      input.checked = mode === "dark";
 
-			input.addEventListener('change', (e) => {
-				let mode = e.target.checked ? 'dark' : 'light';
-				setMode(mode);
-			});
-		},
-	},
+      input.addEventListener("change", (e) => {
+        let mode = e.target.checked ? "dark" : "light";
+        setMode(mode);
+      });
+    },
+  },
 };
 
 let csrfToken = document
-	.querySelector("meta[name='csrf-token']")
-	.getAttribute('content');
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
 
-let liveSocket = new LiveSocket('/live', Socket, {
-	hooks: { ...Hooks, ...getHooks(Components) },
-	longPollFallbackMs: 500,
-	params: { _csrf_token: csrfToken },
+let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: Hooks,
+  longPollFallbackMs: 500,
+  params: { _csrf_token: csrfToken },
 });
 
 // Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' });
-window.addEventListener('phx:page-loading-start', (_info) => topbar.show(300));
-window.addEventListener('phx:page-loading-stop', (_info) => topbar.hide());
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
@@ -92,11 +90,11 @@ liveSocket.connect();
 window.liveSocket = liveSocket;
 
 // Core component socket event functionality
-window.addEventListener('phx:close_popup', (e) => {
-	let id = e.detail.id;
-	let el = document.getElementById(id);
+window.addEventListener("phx:close_popup", (e) => {
+  let id = e.detail.id;
+  let el = document.getElementById(id);
 
-	if (el) {
-		liveSocket.execJS(el, el.getAttribute('data-cancel'));
-	}
+  if (el) {
+    liveSocket.execJS(el, el.getAttribute("data-cancel"));
+  }
 });
--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule Flow.MixProject do
       {:phoenix_live_view, "~> 1.0.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
@@ -50,8 +51,7 @@ defmodule Flow.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"},
-      {:live_svelte, "~> 0.14.0"}
+      {:bandit, "~> 1.5"}
     ]
   end
 
@@ -61,13 +61,9 @@ defmodule Flow.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "cmd --cd assets npm install"],
-      "assets.build": ["tailwind flow", "cmd --cd assets node build.js"],
-      "assets.deploy": [
-        "tailwind flow --minify",
-        "cmd --cd assets node build.js --deploy",
-        "phx.digest"
-      ]
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing", "cmd --cd assets npm install"],
+      "assets.build": ["tailwind default", "esbuild default"],
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
