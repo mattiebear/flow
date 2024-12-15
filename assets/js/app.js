b3730644b43e304f5786901b2db36e58ce00d4e1
Matthew Sells <matthew.sells@proton.me>
2024-12-15 14:15:17 -0600

    get route to display

--- assets/js/app.js
+++ assets/js/app.js
@@ -87,13 +87,7 @@ import { createInertiaApp } from '@inertiajs/svelte';
 import { mount } from 'svelte';
 
 createInertiaApp({
-  resolve: (name) => {
-    const pages = import.meta.glob('./components/pages/**/*.svelte', {
-      eager: true,
-    });
-
-    return pages[`./pages/${name}.svelte`];
-  },
+  resolve: async (name) => await import(`./components/pages/${name}.svelte`),
   setup({ el, App, props }) {
     mount(App, { target: el, props });
   },
